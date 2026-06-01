2914de55ed09583f5b081bdf5c9ea0be09eedfd5
Pranisha Bhatta <pranishabhatta9@gmail.com>
2026-06-01 20:19:56 +0545

    Refactor register screen to StatefulWidget

--- lib/register.dart
+++ lib/register.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
