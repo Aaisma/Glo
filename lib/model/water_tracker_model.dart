7bc88c94a370deb129eb8b84f3c60fb26ff13847
Pranisha Bhatta <pranishabhatta9@gmail.com>
2026-06-09 22:10:17 +0545

    Add WaterIntake class with fields and constructor

---
+++ lib/model/water_tracker_model.dart
@@ -0,0 +1,11 @@
+class WaterIntake {
+  final String id;
+  final double amount;
+  final DateTime date;
+
+  WaterIntake({
+    required this.id,
+    required this.amount,
+    required this.date,
+  });
+}
