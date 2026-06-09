00817e6ecb9c4dac70c0d5daa63064d0d10816c3
Pranisha Bhatta <pranishabhatta9@gmail.com>
2026-06-09 22:24:16 +0545

    Add toMap and fromMap methods

--- lib/model/water_tracker_model.dart
+++ lib/model/water_tracker_model.dart
@@ -1,11 +1,19 @@
 class WaterIntake {
-  final String id;
-  final double amount;
-  final DateTime date;
+  final String id;        // unique ID for each intake entry
+  final double amount;    // amount of water in liters
+  final DateTime date;    // when the water was consumed
 
   WaterIntake({
     required this.id,
     required this.amount,
     required this.date,
   });
-}
+
+  // Convert to Map (for saving later in Firebase)
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'amount': amount,
+      'date': date.toIso8601String(),
+    };
+  }
