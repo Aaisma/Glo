7a7a792e5aa3863a026047ff4edc1eb3df807942
Pranisha Bhatta <pranishabhatta9@gmail.com>
2026-06-13 16:16:25 +0545

    Add Firestore serialization methods to WaterTrackerModel

--- lib/model/water_tracker_model.dart
+++ lib/model/water_tracker_model.dart
@@ -6,4 +6,20 @@ class WaterTrackerModel {
     required this.currentIntake,
     required this.goal,
   });
+
+  // Optional: helps when saving/loading later from Firebase
+  Map<String, dynamic> toMap() {
+    return {
+      'currentIntake': currentIntake,
+      'goal': goal,
+    };
+  }
+
+  // Optional: helps when reading from Firebase later
+  factory WaterTrackerModel.fromMap(Map<String, dynamic> map) {
+    return WaterTrackerModel(
+      currentIntake: (map['currentIntake'] ?? 0).toDouble(),
+      goal: (map['goal'] ?? 2.5).toDouble(),
+    );
+  }
 }
