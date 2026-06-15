05387d5a201537cf744c474f58191efe00383557
Aaisma <wagleaaisma07@gmail.com>
2026-06-15 15:03:42 +0545

    Clean branch before merging.

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,8 @@ import 'firebase_options.dart';
 import 'view/glo_splash_screen.dart';
 import 'view/navigation_icon/dashboard_page.dart';
 import 'view/survey_page.dart';
-import 'view/dashboard_card/ovulation_period_page.dart';
-import 'package:provider/provider.dart';
-import 'view/dashboard_card/log_symptoms_page.dart';
-import 'repo/period_repo_impl.dart';
-import 'repo/ovulation_repo_impl.dart';
-import 'viewmodel/period_view_model.dart';
-import 'viewmodel/ovulation_view_model.dart';
+
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -25,19 +18,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => PeriodViewModel(PeriodRepoImpl())),
-        ChangeNotifierProvider(create: (_) => OvulationViewModel(OvulationRepoImpl())),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Again Project',
-        theme: ThemeData(
-          primarySwatch: Colors.pink,
-        ),
-        home: const OvulationPage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Again Project',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
       ),
+      home: const SurveyPage(),
+      routes: {
+        '/dashboard': (context) => const DashboardScreen(),
+        '/survey': (context) => const SurveyPage(),
+      },
     );
   }
 }
