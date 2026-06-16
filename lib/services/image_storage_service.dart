c4826572ce0b2e46dccf434afc6ace963e687def
Pranisha Bhatta <pranishabhatta9@gmail.com>
2026-06-16 20:34:14 +0545

    Image storage service for Firebase upload and URL retrieval

---
+++ lib/services/image_storage_service.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class ImageStorageService {
+  final FirebaseStorage storage = FirebaseStorage.instance;
+
+  Future<String> uploadImage(File file, String uid, String date) async {
+    final ref = storage.ref().child("acne/$uid/$date.jpg");
+
+    await ref.putFile(file);
+
+    final url = await ref.getDownloadURL();
+    return url;
+  }
+}
