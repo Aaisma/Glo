82640d480592fd040a24a53ea8e686bbbbaf8250
Pranisha Bhatta <pranishabhatta9@gmail.com>
2026-06-29 07:28:37 +0545

    Add meal tracker feature with MVVM architecture

---
+++ lib/main_meal_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'repo/meal_repo_impl.dart';
+import 'viewmodel/meal_tracker_viewmodel.dart';
+import 'view/meal_tracker_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MealTestApp());
+}
+
+class MealTestApp extends StatelessWidget {
+  const MealTestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MealTrackerViewModel(MealRepoImpl())),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MealTrackerScreen(),
+      ),
+    );
+  }
+}
