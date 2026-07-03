88eb6ef437f2c55217bff5aa1d2897a9103f1c57
ayushasapkota <ayushasapkota108@gmail.com>
2026-07-03 17:45:26 +0545

    Pulled only top and bottom navigation from development_branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-<<<<<<< HEAD
 
 import 'firebase_options.dart';
 
@@ -44,52 +42,12 @@ import 'package:glo/view/authentication/glo_otp.dart';
 import 'package:glo/view/health_overview_screen.dart';
 
 Future<void> main() async {
-=======
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-import 'view/glo_splash_screen.dart';
-import 'view/authentication/auth_wrapper.dart';
-import 'view/authentication/authentication_page.dart';
-import 'view/authentication/login_screen.dart';
-import 'view/authentication/register_screen.dart';
-import 'view/survey_page.dart';
-import 'view/navigation_icon/glo_profile.dart';
-import 'view/dashboard_page.dart';
-import 'view/admin/admin_dashboard_page.dart';
-
-// Repos
-import 'repo/user_repo_impl.dart';
-import 'repo/auth_repo_impl.dart';
-import 'repo/period_repo_impl.dart';
-import 'repo/ovulation_repo_impl.dart';
-import 'repo/insights_repo_impl.dart';
-import 'repo/community_repo_impl.dart';
-import 'repo/insights_moderation_repo_impl.dart';
-import 'repo/community_moderation_repo_impl.dart';
-import 'repo/acne_repo_impl.dart';
-
-// ViewModels
-import 'viewmodel/user_view_model.dart';
-import 'viewmodel/auth_view_model.dart';
-import 'viewmodel/period_view_model.dart';
-import 'viewmodel/ovulation_view_model.dart';
-import 'viewmodel/insight_view_model.dart';
-import 'viewmodel/community_view_model.dart';
-import 'viewmodel/moderation_view_model.dart';
-import 'viewmodel/tracker_navigation_view_model.dart';
-import 'viewmodel/acne_tracker_viewmodel.dart';
-
-void main() async {
->>>>>>> 833f232 (Updated top and bottom navigation to modify calender placement.)
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-<<<<<<< HEAD
   runApp(const GloApp());
 }
 
@@ -184,30 +142,11 @@ class GloApp extends StatelessWidget {
     final phone = _getPhoneFromRoute(context);
     return GloOtpScreen(phone: phone);
   }
-=======
-  // Initialize Hive
-  await Hive.initFlutter();
-  await Hive.openBox('insights_box');
-  await Hive.openBox('community_box');
-  await Hive.openBox('insights_moderation_box');
-  await Hive.openBox('community_moderation_box');
-  await Hive.openBox('drafts_box');
-  await Hive.openBox('favorites_box');
-  await Hive.openBox('hidden_content_box');
-  await Hive.openBox('onboarding_box');
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
->>>>>>> 833f232 (Updated top and bottom navigation to modify calender placement.)
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-<<<<<<< HEAD
         ChangeNotifierProvider<MedicationViewModel>(
           create: (_) => MedicationViewModel(),
         ),
@@ -283,88 +222,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-=======
-        ChangeNotifierProvider(create: (_) => UserViewModel(userRepo: UserRepoImpl())),
-        ChangeNotifierProvider(create: (_) => AuthViewModel(authRepo: AuthRepoImpl(), userRepo: UserRepoImpl())),
-        ChangeNotifierProvider(create: (_) => PeriodViewModel(PeriodRepoImpl())),
-        ChangeNotifierProvider(create: (_) => OvulationViewModel(OvulationRepoImpl())),
-        ChangeNotifierProvider(create: (_) => TrackerNavigationViewModel()),
-        ChangeNotifierProvider(create: (_) => AcneTrackerViewModel(AcneRepoImpl())),
-
-        // Insights Module ViewModels
-        ChangeNotifierProvider(create: (_) => InsightsFeedViewModel(InsightsRepoImpl())),
-        ChangeNotifierProvider(create: (_) => FavoritesViewModel(InsightsRepoImpl(), CommunityRepoImpl())),
-        ChangeNotifierProvider(create: (_) => CreateInsightViewModel(InsightsRepoImpl())),
-        ChangeNotifierProvider(create: (_) => InsightsLibraryViewModel(InsightsRepoImpl())),
-        ChangeNotifierProvider(create: (_) => ArticleDetailViewModel(InsightsRepoImpl(), InsightsModerationRepoImpl())),
-
-        // Community Module ViewModels
-        ChangeNotifierProvider(create: (_) => CommunityFeedViewModel(CommunityRepoImpl())),
-        ChangeNotifierProvider(create: (_) => DiscussionDetailViewModel(CommunityRepoImpl(), CommunityModerationRepoImpl())),
-        ChangeNotifierProvider(create: (_) => CreateDiscussionViewModel(CommunityRepoImpl())),
-        ChangeNotifierProvider(create: (_) => CreatePollViewModel(CommunityRepoImpl())),
-        ChangeNotifierProvider(create: (_) => CommunityLibraryViewModel(CommunityRepoImpl())),
-
-        // Separated Moderation ViewModels
-        ChangeNotifierProvider(create: (_) => InsightsModerationQueueViewModel(InsightsModerationRepoImpl())),
-        ChangeNotifierProvider(create: (_) => InsightsModerationDetailViewModel(InsightsModerationRepoImpl())),
-        ChangeNotifierProvider(create: (_) => CommunityModerationQueueViewModel(CommunityModerationRepoImpl())),
-        ChangeNotifierProvider(create: (_) => CommunityModerationDetailViewModel(CommunityModerationRepoImpl())),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Glo Project',
-        theme: ThemeData(
-          primarySwatch: Colors.pink,
-          scaffoldBackgroundColor: Colors.white,
-        ),
-        home: const SplashScreen(),
-        routes: {
-          '/splash': (context) => const SplashScreen(),
-          '/authWrapper': (context) => const AuthWrapper(),
-          '/authentication': (context) => const AuthenticationPage(),
-          '/login': (context) => const LoginScreen(),
-          '/register': (context) => const RegisterScreen(),
-          '/survey': (context) => const SurveyPage(),
-          '/gloProfile': (context) => const GloProfileScreen(),
-          '/dashboard': (context) => const DashboardScreen(),
-          '/adminDashboard': (context) => const AdminDashboardPage(),
-        },
-      ),
-    );
-  }
-}
-
-class AuthGate extends StatefulWidget {
-  const AuthGate({super.key});
-
-  @override
-  State<AuthGate> createState() => _AuthGateState();
-}
-
-class _AuthGateState extends State<AuthGate> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      final authVM = Provider.of<AuthViewModel>(context, listen: false);
-      if (authVM.user != null) {
-        // await authVM.checkUserProfile(context, authVM.user!.uid);
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final authVM = Provider.of<AuthViewModel>(context);
-    if (authVM.user != null) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-    return const AuthenticationPage();
-  }
-}
->>>>>>> 833f232 (Updated top and bottom navigation to modify calender placement.)
